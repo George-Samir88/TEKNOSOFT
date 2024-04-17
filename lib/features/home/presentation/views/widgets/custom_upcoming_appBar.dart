0d21687d106292257ddbf6014aba9faf0a6b98f4
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2024-04-17 16:24:53 +0200

    finish upcoming appBar

--- lib/features/home/presentation/views/widgets/custom_upcoming_appBar.dart
+++ lib/features/home/presentation/views/widgets/custom_upcoming_appBar.dart
@@ -8,10 +8,21 @@ class CustomUpcomingAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         IconButton(
-          onPressed: () {},
+          onPressed: () {
+            Navigator.pop(context);
+          },
           icon: const Icon(Icons.arrow_back_ios_new_outlined),
+        ),
+        const Text(
+          'Upcoming',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.more_horiz_rounded),
         ),
       ],
     );
