e79517c7bd29ec4ba3edcdf4975d8d7de56e4fb7
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2024-04-18 02:55:42 +0200

    add table of dates

---
+++ lib/features/home/presentation/views/widgets/calendar_widget.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class CalendarView extends StatelessWidget {
+  const CalendarView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Table(
+      children: [
+        TableRow(
+          children: List.generate(
+            7,
+            (index) => TableCell(
+              child: Center(
+                child: index == 3
+                    ? Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            gradient: const LinearGradient(
+                              colors: [
+                                Color(0xff6397A6),
+                                Color(0xff4D65BC),
+                              ],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                            )),
+                        child: Column(
+                          children: [
+                            Text(
+                              _getAbbreviatedDayOfWeek(index),
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.w400, fontSize: 12),
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              _getDayOfWeek(index),
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.w400, fontSize: 12),
+                            ),
+                          ],
+                        ),
+                      )
+                    : Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              _getAbbreviatedDayOfWeek(index),
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.w400, fontSize: 12),
+                            ),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              _getDayOfWeek(index),
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.w400, fontSize: 12),
+                            ),
+                          ],
+                        ),
+                      ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  String _getAbbreviatedDayOfWeek(int index) {
+    final dateTimeLast3Days = DateTime.now().subtract(const Duration(days: 3));
+    return DateFormat.E()
+        .format(
+          dateTimeLast3Days.add(
+            Duration(days: index),
+          ),
+        )
+        .toString()
+        .substring(0, 1); // E gives abbreviated day name (e.g., Mon, Tue, etc.)
+  }
+
+  String _getDayOfWeek(int index) {
+    var dateTimeLast3Days = DateTime.now().subtract(
+      const Duration(days: 3),
+    );
+    return dateTimeLast3Days
+        .add(Duration(days: index))
+        .toString()
+        .substring(8, 10);
+    // return DateTime.now()
+    //     .subtract(Duration(days: DateTime.now().weekday - index - 1))
+    //     .toString()
+    //     .substring(8, 10);
+  }
+}
--- lib/features/home/presentation/views/widgets/upcoming_view_body.dart
+++ lib/features/home/presentation/views/widgets/upcoming_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_list_teknosoft/features/home/presentation/views/widgets/calendar_widget.dart';
 import 'package:to_do_list_teknosoft/features/home/presentation/views/widgets/custom_background.dart';
 
 import 'custom_upcoming_appBar.dart';
@@ -32,7 +33,7 @@ class UpcomingViewBody extends StatelessWidget {
                 SizedBox(
                   height: 12,
                 ),
-
+                CalendarView(),
               ],
             ),
           ),
