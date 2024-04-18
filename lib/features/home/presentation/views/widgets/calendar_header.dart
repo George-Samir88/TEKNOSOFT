87c32e571cdec68241d10fedc19e3aa3f60b40a5
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2024-04-18 03:55:04 +0200

    add date picker

--- lib/features/home/presentation/views/widgets/calendar_header.dart
+++ lib/features/home/presentation/views/widgets/calendar_header.dart
@@ -2,22 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:to_do_list_teknosoft/features/home/presentation/views/widgets/my_day_view.dart';
 
-class CalendarHeader extends StatelessWidget {
+class CalendarHeader extends StatefulWidget {
   const CalendarHeader({super.key});
+
+  @override
+  State<CalendarHeader> createState() => _CalendarHeaderState();
+}
+
+class _CalendarHeaderState extends State<CalendarHeader> {
+  DateTime? _selectedDate;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         Text(
-          "${getMonthName()} ${DateTime.now().year}",
+          "${getMonthName(_selectedDate)} ${_selectedDate?.year ?? DateTime.now().year}",
           style: const TextStyle(
             fontSize: 16,
             fontWeight: FontWeight.w500,
           ),
         ),
         IconButton(
-          onPressed: () {},
+          onPressed: () {
+            _pickDate(context);
+          },
           icon: Transform.rotate(
             angle: -1.57079633,
             child: const Icon(
@@ -49,10 +58,26 @@ class CalendarHeader extends StatelessWidget {
     );
   }
 
-  String getMonthName() {
-    DateTime dateTime = DateTime.now();
-    return DateFormat.MMM().format(
-      dateTime,
+  Future<void> _pickDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: _selectedDate ?? DateTime.now(),
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2030),
     );
+    if (picked != null && picked != _selectedDate) {
+      setState(() {
+        _selectedDate = picked;
+      });
+    }
+  }
+
+  String getMonthName(DateTime? dateTime) {
+    DateTime dateTimeNow = DateTime.now();
+    return DateFormat.MMM()
+        .format(
+          dateTime ?? dateTimeNow,
+        )
+        .toString();
   }
 }
